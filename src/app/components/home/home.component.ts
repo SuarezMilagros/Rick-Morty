ba95fca272f72350842b11b9db65e974a527a1ae
SuarezMilagros <milagros.16.princesa@gmail.com>
2024-11-02 09:41:55 -0300

    Cargue la Api al proyecto

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from 'src/app/service/rick-and-morty.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+  
+  characters: any[] = [];
+
+  constructor(private service: RickAndMortyService){}
+
+  async ngOnInit(): Promise<void> {
+    const data = await this.service.getCharacters().toPromise();
+    this.characters = data.results;
+    /*this.service.getCharacters().subscribe(data => {
+        this.characters = data.results;
+    });*/
+  }
+
+
+
+}
